599f9072bea02b5ec40ddc148b6e0077d50670f3
John A. Samuel <144686157+CodeGallantX@users.noreply.github.com>
2025-08-15 21:23:53 +0100

    Updated index.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ThemeProvider } from './context/ThemeContext';
 import { AuthProvider } from './context/AuthContext';
-import { AppProvider } from './context/AppContext';
+import { OnboardingProvider } from './context/OnboardingContext';
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 5 * 60 * 1000, // 5 minutes
-      cacheTime: 10 * 60 * 1000, // 10 minutes
-      retry: 1,
-      refetchOnWindowFocus: false,
-    },
-  },
-});
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider>
-        <AppProvider>
-          <AuthProvider>
-            <App />
-          </AuthProvider>
-        </AppProvider>
-        <ReactQueryDevtools initialIsOpen={false} />
-      </ThemeProvider>
-    </QueryClientProvider>
-  </React.StrictMode>,
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ThemeProvider>
+      <AuthProvider>
+        <OnboardingProvider>
+          <App />
+        </OnboardingProvider>
+      </AuthProvider>
+    </ThemeProvider>
+  </StrictMode>
 );
